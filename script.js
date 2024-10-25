6d40643a3511c510d264c12f6f3fab72d37716cd
Krissia Mendez <krissiamendez67@gmail.com>
2024-10-25 07:55:20 -0600

    segundo commit

---
+++ script.js
@@ -0,0 +1,15 @@
+ // Selecciona el menú y el encabezado del html y Style.css
+ const menu = document.querySelector("nav");
+ const header = document.querySelector("#mi-encabezado");
+
+ // Obtén la altura del encabezado
+ const headerHeight = header.offsetHeight;
+
+ // Función que cambia el color de fondo del menú cuando se desplaza
+ window.addEventListener("scroll", () => {
+     if (window.scrollY > headerHeight) {
+         menu.style.backgroundColor = "rgba(0, 218, 200)"; // Cambia el color de fondo a azul
+     } else {
+         menu.style.backgroundColor = "rgba(0, 218, 200)"; // Vuelve al fondo transparente
+     }
+ });
